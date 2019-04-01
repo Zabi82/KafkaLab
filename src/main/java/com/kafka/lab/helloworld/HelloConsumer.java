8ad1d8407b98d7ed3bff240aee29954fc37f7f9a
Zabeer <mail2zabeer@gmail.com>
2019-04-02 00:32:49 +0800

    added pom.xml and skeleton producer and consumer

---
+++ src/main/java/com/kafka/lab/helloworld/HelloConsumer.java
@@ -0,0 +1,48 @@
+package com.kafka.lab.helloworld;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Properties;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.TopicPartition;
+import org.apache.kafka.common.serialization.StringDeserializer;
+
+public class HelloConsumer {
+
+	public void consumeMessages(int timeout) {
+
+		Properties props = new Properties();
+		props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, "testgroup");
+        props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "true");
+        props.put(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, "1000");
+        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+
+     // TODO: Create a new consumer, with the properties we've created above
+
+        // TODO: Subscribe to your topic here
+
+        while (true) {
+            // TODO: Read records using the poll() method 
+
+            // TODO: Loop around the records, printing out each record.offset, record.key, and record.value
+        }
+	}
+	
+	public static void main(String[] args) {
+		HelloConsumer consumer = new HelloConsumer();
+		consumer.consumeMessages(100);
+	}
+
+}
---
+++ src/main/java/com/kafka/lab/helloworld/HelloProducer.java
@@ -0,0 +1,45 @@
+package com.kafka.lab.helloworld;
+
+import java.util.Properties;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.requests.ProduceRequest;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.apache.log4j.chainsaw.Main;
+
+public class HelloProducer {
+	
+	public static final String HELLO_TOPIC = "hello_world_topic";
+	
+	public void produceMessage(int numMessages) {
+		
+		Properties props = new Properties();
+		props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+		props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+		props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+		
+		//TODO: Create a KafkaProducer
+
+		
+		String key = "Key";
+		String value = "Value";
+		for(int i=0; i < numMessages; i++) {
+			// TODO: Create the ProducerRecord
+
+            // TODO: Write the record
+
+            // TODO: Print the key and value
+		}
+		
+		//TODO: Close the producer
+		
+	}
+	
+	public static void main(String[] args) {
+		HelloProducer producer = new HelloProducer();
+		producer.produceMessage(10);
+	}
+}
